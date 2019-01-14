9a467eb7b05a0e830af6be81d4aabec116e6cf33
dvld <nikoroberts620@gmail.com>
2019-01-14 14:42:13 -0600

    created app

---
+++ my-app/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// jsx
+ReactDOM.render(<div><h1>Hello World!</h1><p>this is a paragraph</p></div>, document.getElementById('root'));
